965edcc92e84b65d2537eb42bfb9ed9bb3951f4b
Rudrajeet Chaudhuri <114332463+RudeDaCode@users.noreply.github.com>
2026-07-29 11:53:30 +0530

    Injects errors into synthetic data

--- generate_data.py
+++ generate_data.py
@@ -1,7 +1,102 @@
 from faker import Faker
+import pandas as pd
+import random
 
 fake = Faker()
+MERCHANTS = [
+    "Amazon",
+    "Flipkart",
+    "Thuisbezorgd",
+    "Uber",
+    "Netflix",
+    "Apple",
+    "Steam",
+    "Nike"
+]
 
-print(fake.name())
-print(fake.country())
-print(fake.company())
+PAYMENT_METHODS = [
+    "Credit Card",
+    "Debit Card",
+    "Net Banking"
+]
+
+CURRENCIES = [
+    "INR",
+    "EUR",
+    "USD"
+]
+
+DEVICES = [
+    "Mobile",
+    "Desktop",
+    "Tablet"
+]
+CATEGORIES = [
+    "Shopping",
+    "Food",
+    "Transport",
+    "Entertainment",
+    "Healthcare",
+    "Travel",
+    "Bills"
+]
+COUNTRIES = [
+    "India",
+    "Netherlands",
+    "Germany",
+    "United Kingdom",
+    "United States"
+]
+
+
+def generate_transaction(transaction_id):
+    return {
+        "transaction_id": f"TXN{transaction_id:06}",
+        "customer_id": f"CUST{random.randint(1000, 9999)}",
+        "timestamp": fake.date_time_this_year(),
+        "merchant": random.choice(MERCHANTS),
+        "amount": round(random.uniform(5, 5000), 2),
+        "currency": random.choice(CURRENCIES),
+        "payment_method": random.choice(PAYMENT_METHODS),
+        "country": random.choice(COUNTRIES),
+        "device": random.choice(DEVICES),
+        "category": random.choice(CATEGORIES),
+    }
+
+transactions = []
+#create synthetic data
+for i in range(1, 100001):
+    transactions.append(generate_transaction(i))
+
+df = pd.DataFrame(transactions)
+#inject errors
+merchant_mask = df.sample(frac=0.01).index
+df.loc[merchant_mask, "merchant"] = None
+customer_mask = df.sample(frac=0.005).index
+df.loc[customer_mask, "customer_id"] = None
+#negative transactions
+amount_mask = df.sample(frac=0.005).index
+df.loc[amount_mask, "amount"] *= -1
+#mapping errors
+currency_mask = df.sample(frac=0.003).index
+df.loc[currency_mask, "currency"] = "XYZ"
+#invalid payment method
+payment_mask = df.sample(frac=0.003).index
+df.loc[payment_mask, "payment_method"] = "Crypto Potato"
+#duplicate rows
+duplicates = df.sample(frac=0.02)
+df = pd.concat([df, duplicates], ignore_index=True)
+'''
+print("Rows:", len(df))
+print("Missing merchants:", df["merchant"].isna().sum())
+print("Missing customers:", df["customer_id"].isna().sum())
+print("Negative amounts:", (df["amount"] < 0).sum())
+print("Invalid currencies:", (df["currency"] == "XYZ").sum())
+print("Invalid payment methods:", (df["payment_method"] == "Crypto Potato").sum())
+print("Duplicate rows:", df.duplicated().sum())
+'''
+#save dataset
+
+df.to_csv("data/raw/transactions.csv", index=False)
+
+print(f"Generated {len(df)} transactions.")
